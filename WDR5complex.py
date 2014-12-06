Made some modifications to the WDR5 model: 1) added a volume and changed all units to molecule units; 2) fixed an error in how the dissociation rate constants were being calculated; 3) general code cleanup.

WDR5complex.py
@@ -4,125 +4,63 @@ from numpy import linspace
 import matplotlib.pyplot as plt
 from pysb.bng import run_ssa
 from pysb.integrate import odesolve
+import scipy.constants as sc
 
 Model() 
 
-#Binding domain names are taken from the summary model found in the 
-#Dias, Nguyen, Georgiev et al., 2014 article
+# Binding domain names are taken from the summary model found in the 
+# Dias, Nguyen, Georgiev et al., 2014 article
 
-#Individual protein/protein complexes that are known to associate with WDR5 as 
-#a trimeric complex, including WDR5
-#Abbreviations: 'b'= bound, 'u'= unbound
+VOL = 1e-12 # L (1 picoliter is typical volume of eukaryotic cell)
 
-Monomer('WDR5',['s1'])
-#or Monomer('WDR5',['s1','s2'])
-Monomer('Myc',['DVDL'])
-Monomer('RbBP5',['DVDL'])
-Monomer('KANSL2',['DVDL'])
-#Monomer('MLL',['WIN'])
-#Monomer('KANSL1',['WIN'])
+Monomer('WDR5',   ['s1'])
+Monomer('Myc',    ['DVDL'])
+Monomer('RbBP5',  ['DVDL'])
+Monomer('KANSL2', ['DVDL'])
 
-kd_WDR5_Myc = 2.7e-2
-kd_WDR5_RbBP5 = 5.6e-2
-kd_WDR5_KANSL2 = 8.6e-2
-kdimf = 5.0e-2
-kdimr_WDR5_Myc = kdimf/kd_WDR5_Myc
-kdimr_WDR5_RbBP5 = kdimf/kd_WDR5_RbBP5
-kdimr_WDR5_KANSL2 = kdimf/kd_WDR5_KANSL2
-print kdimr_WDR5_Myc, kdimr_WDR5_RbBP5, kdimr_WDR5_KANSL2
+# Dissocation constants (from experiment)
+Kd_WDR5_Myc    = 2.7*1e-6*sc.N_A*VOL # uM -> molecules
+Kd_WDR5_RbBP5  = 5.6*1e-6*sc.N_A*VOL # uM -> molecules
+Kd_WDR5_KANSL2 = 8.6*1e-6*sc.N_A*VOL # uM -> molecules
 
-#Forward binding rates are set to 50 uM^-2 * s^-1.
-Parameter('kf_WDR5_Myc',kdimf)
-Parameter('kf_WDR5_RbBP5',kdimf)
-Parameter('kf_WDR5_KANSL2',kdimf)
-#Parameter('kf_WDR5_MLL',kdimf)
-#Parameter('kf_WDR5_KANSL1',kdimf)
-#?Parameter('kf_RbBP5_WDR5_Myc',kdimf)
-#?Parameter('kf_RbBP5_WDR5_MLL',kdimf)
-#?Parameter('kf_KANSL2_WDR5_MLL',kdimf)
-#?Parameter('kf_KANSL2_WDR5_KANSL1',kdimf)
-#?Parameter('kf_RbBP5_WDR5_KANSL1',kdimf)
-#?Parameter('kf_KANSL2_WDR5_Myc',kdimf)
-#?Parameter('kf_KANSL2_WDR5_MLL',kdimf)
-#?Parameter('kf_MLL_WDR5_RbBP5',kdimf)
-#?Parameter('kf_Myc_WDR5_RbBP5',kdimf)
+# Forward binding rate constants in units of /s
+Parameter('kf_WDR5_Myc',    1e-6)
+Parameter('kf_WDR5_RbBP5',  1e-6)
+Parameter('kf_WDR5_KANSL2', 1e-6)
 
-#Reverse binding rates are in units uM^-1 * s^-1
-Parameter('kr_WDR5_Myc',kdimf/kd_WDR5_Myc)
-Parameter('kr_WDR5_RbBP5',kdimf/kd_WDR5_RbBP5)
-Parameter('kr_WDR5_KANSL2',kdimf/kd_WDR5_KANSL2)
+# Reverse binding rates are in units of /s
+Parameter('kr_WDR5_Myc',    kf_WDR5_Myc.value    * Kd_WDR5_Myc)
+Parameter('kr_WDR5_RbBP5',  kf_WDR5_RbBP5.value  * Kd_WDR5_RbBP5)
+Parameter('kr_WDR5_KANSL2', kf_WDR5_KANSL2.value * Kd_WDR5_KANSL2)
 
-#Reverse binding rates that have yet to be determined
-#Parameter('kr_WDR5_MLL',kdimf/kd_WDR5_MLL)
-#Parameter('kr_WDR5_KANSL1',kdimf/kd_WDR5_KANSL1)
-#?Parameter('kr_RbBP5_WDR5_Myc',kdimf/kd_RbBP5_WDR5_Myc)
-#?Parameter('kr_RbBP5_WDR5_MLL',kdimf/kd_RbBP5_WDR5_MLL)
-#?Parameter('kr_KANSL2_WDR5_MLL',kdimf/kd_KANSL2_WDR5_MLL)
-#?Parameter('kr_KANSL2_WDR5_KANSL1',kdimf/kd_KANSL2_WDR5_KANSL1)
-#?Parameter('kr_RbBP5_WDR5_KANSL1',kdimf/kd_RbBP5_WDR5_KANSL12)
-#?Parameter('kr_KANSL2_WDR5_Myc',kdimf/kd_KANSL2_WDR5_Myc)
-#?Parameter('kr_KANSL2_WDR5_MLL',kdimf/kd_KANSL2_WDR5_MLL)
-#?Parameter('kr_MLL_WDR5_RbBP5',kdimf/kd_MLL_WDR5_RbBP5)
-#?Parameter('kr_Myc_WDR5_RbBP5',kdimf/kd_Myc_WDR5_RbBP5)
+# The initial protein levels (uM -> molecules) are set to the estimates provided by Dr. Tansey. 
+Parameter('WDR5_0',   round(2.657e-02*1e-6*sc.N_A*VOL)) # 16000
+Parameter('Myc_0',    round(8.303e-03*1e-6*sc.N_A*VOL)) # 5000
+Parameter('RbBP5_0',  round(7.473e-03*1e-6*sc.N_A*VOL)) # 4500
+Parameter('KANSL2_0', round(3.321e-03*1e-6*sc.N_A*VOL)) # 2000
 
-#The parameters are set to the molecular estimates provided by Dr. Tansey. 
-#Will need to determine quantity of the number of molecules by 
-#experimentation.
-Parameter('WDR5_0',16000)
-Parameter('Myc_0',5000)
-Parameter('RbBP5_0',4500)
-Parameter('KANSL2_0',2000)
-#Parameter('MLL_0',2e3)
-#Parameter('KANSL1_0',2e3)
-
-
-#All monomers are initially described in the unbound state at all possible
-#binding sites.
-Initial(WDR5(s1=None), WDR5_0)
-Initial(Myc(DVDL=None), Myc_0)
-Initial(RbBP5(DVDL=None), RbBP5_0)
+# Initial conditions
+Initial(WDR5(s1=None),     WDR5_0)
+Initial(Myc(DVDL=None),    Myc_0)
+Initial(RbBP5(DVDL=None),  RbBP5_0)
 Initial(KANSL2(DVDL=None), KANSL2_0)
-#Initial(MLL(WIN=None), MLL_0)
-#Initial(KANSL1(WIN=None), KANSL1_0)
 
-
-#Rules for Myc binding WDR5
-Rule('WDR5_binds_Myc', WDR5(s1=None) + Myc(DVDL=None) <> WDR5(s1=1) % Myc(DVDL=1), kf_WDR5_Myc, kr_WDR5_Myc)
-#Rule('WDR5_binds_Myc', WDR5(s1=None,s2=None) + Myc(WIN=None) <> WDR5(s1=1,s2=None) % Myc(WIN=1), kf_WDR5_Myc, kr_WDR5_Myc)
-#Rule('WDR5_RbBP5_binds_Myc', WDR5(s1=None,s2=1) % RbBP5(DVDL=1) + Myc(WIN=None) <> WDR5(s1=2,s2=1) % RbBP5(DVDL=1) % Myc(WIN=2), kf_RbBP5_WDR5_Myc, kr_RbBP5_WDR5_Myc)
-#Rule('WDR5_KANSL2_binds_Myc', WDR5(s1=None,s2=1) % KANSL2(DVDL=1) + Myc(WIN=None) <> WDR5(s1=2,s2=1) % KANSL2(DVDL=1) % Myc(WIN=2), kf_KANSL2_WDR5_Myc, kr_KANSL2_WDR5_Myc)
-
-#Rules for RbBP5 binding WDR5
-Rule('WDR5_binds_RbBP5', WDR5(s1=None) + RbBP5(DVDL=None) <> WDR5(s1=1) % RbBP5(DVDL=1), kf_WDR5_RbBP5, kr_WDR5_RbBP5)
-#Rule('WDR5_binds_RbBP5', WDR5(s1=None,s2=None) + RbBP5(DVDL=None) <> WDR5(s1=None,s2=1) % RbBP5(DVDL=1), kf_WDR5_RbBP5, kr_WDR5_RbBP5)
-#Rule('WDR5_MLL_binds_RbBP5', WDR5(s1=1,s2=None) % MLL(WIN=1) + RbBP5(DVDL=None) <> WDR5(s1=1,s2=2) % MLL(WIN=1) + RbBP5(DVDL=2), kf_MLL_WDR5_RbBP5, kr_MLL_WDR5_RbBP5)
-#Rule('WDR5_Myc_binds_RbBP5', WDR5(s1=1,s2=None) % Myc(WIN=1) + RbBP5(DVDL=None) <> WDR5(s1=1,s2=2) % Myc(WIN=1) + RbBP5(DVDL=2), kf_Myc_WDR5_RbBP5, kr_Myc_WDR5_RbBP5)
-
-#Rules for KANSL2 binding WDR5
+# Reversible binding rules
+Rule('WDR5_binds_Myc',    WDR5(s1=None) + Myc(DVDL=None)    <> WDR5(s1=1) % Myc(DVDL=1),    kf_WDR5_Myc,    kr_WDR5_Myc)
+Rule('WDR5_binds_RbBP5',  WDR5(s1=None) + RbBP5(DVDL=None)  <> WDR5(s1=1) % RbBP5(DVDL=1),  kf_WDR5_RbBP5,  kr_WDR5_RbBP5)
 Rule('WDR5_binds_KANSL2', WDR5(s1=None) + KANSL2(DVDL=None) <> WDR5(s1=1) % KANSL2(DVDL=1), kf_WDR5_KANSL2, kr_WDR5_KANSL2)
-#Rule('WDR5_binds_KANSL2', WDR5(s1=None,s2=None) + KANSL2(DVDL=None) <> WDR5(s1=None,s2=1) % RbBP5(DVDL=1), kf_WDR5_KANSL2, kr_WDR5_KANSL2)
-#Rule('WDR5_MLL_binds_KANSL2', WDR5(s1=1,s2=None) % MLL(WIN=1) + KANSL2(DVDL=None) <> WDR5(s1=1,s2=2) % MLL(WIN=1) % KANSL2(DVDL=2), kf_MLL_WDR5_KANSL2, kr_MLL_WDR5_KANSL2)
-#Rule('WDR5_Myc_binds_KANSL2', WDR5(s1=1,s2=None) % Myc(WIN=1) + KANSL2(DVDL=None) <> WDR5(s1=1,s2=2) % Myc(WIN=1) % KANSL2(DVDL=2), kf_Myc_WDR5_KANSL2, kr_Myc_WDR5_KANSL2)
-#Rule('WDR5_KANSL1_binds_KANSL2', WDR5(s1=1,s2=None) % KANSL1(WIN=1) + KANSL2(DVDL=None) <> WDR5(s1=1,s2=2) % KANSL1(WIN=1) % KANSL2(DVDL=2), kf_KANSL1_WDR5_KANSL2, kr_KANSL1_WDR5_KANSL2)
 
-#Rules for MLL binding WDR5
-#Rule('WDR5_binds_MLL', WDR5(s1=None) + MLL(WIN=None) <> WDR5(s1=1) % MLL(WIN=1), kf_WDR5_MLL, kr_WDR5_MLL)
-#Rule('WDR5_binds_MLL', WDR5(s1=None,s2=None) + MLL(WIN=None) <> WDR5(s1=1,s2=None) % Myc(WIN=1), kf_WDR5_MLL, kr_WDR5_MLL)
-#Rule('WDR5_RbBP5_binds_MLL', WDR5(s1=None,s2=1) % RbBP5(DVDL=1) + MLL(WIN=None) <> WDR5(s1=2,s2=1) % RbBP5(DVDL=1) % MLL(WIN=2), kf_RbBP5_WDR5_MLL, kr_RbBP5_WDR5_MLL)
-#Rule('WDR5_KANSL2_binds_MLL', WDR5(s1=None,s2=1) % KANSL2(DVDL=1) + MLL(WIN=None) <> WDR5(s1=2,s2=1) % KANSL2(DVDL=1) % MLL(WIN=2), kf_KANSL2_WDR5_MLL, kr_KANSL2_WDR5_MLL)
-
-#Rules for KANSL1 binding WDR5
-#Rule('WDR5_binds_KANSL1', WDR5(s1=None) + KANSL1(WIN=None) <> WDR5(s1=1) % KANSL1(WIN=1), kf_WDR5_KANSL1, kr_WDR5_KANSL1)
-#Rule('WDR5_binds_KANSL1', WDR5(s1=None,s2=None) + KANSL1(WIN=None) <> WDR5(s1=1,s2=None) % KANSL1(WIN=1), kf_WDR5_KANSL1, kr_WDR5_KANSL1)
-#Rule('WDR5_RbBP5_binds_KANSL1', WDR5(s1=None,s2=1) % RbBP5(DVDL=1) + KANSL1(WIN=None) <> WDR5(s1=2,s2=1) % RbBP5(DVDL=1) % KANSL1(WIN=2), kf_RbBP5_WDR5_KANSL1, kr_RbBP5_WDR5_KANSL1)
-#Rule('WDR5_KANSL2_binds_KANSL1', WDR5(s1=None,s2=1) % KANSL2(DVDL=1) + KANSL1(WIN=None) <> WDR5(s1=2,s2=1) % KANSL2(DVDL=1) % KANSL1(WIN=2), kf_KANSL2_WDR5_KANSL1, kr_KANSL2_WDR5_KANSL1)
-
-#Observable species
-Observable('WDR5_free', WDR5(s1=None))
-Observable('WDR5_Myc', WDR5(s1=1) % Myc(DVDL=1))
-Observable('WDR5_RbBP5', WDR5(s1=1) % RbBP5(DVDL=1))
+# Observables
+# Free species
+Observable('WDR5_free',   WDR5(s1=None))
+Observable('Myc_free',    Myc(DVDL=None))
+Observable('RbBP5_free',  RbBP5(DVDL=None))
+Observable('KANSL2_free', KANSL2(DVDL=None))
+# Complexes
+Observable('WDR5_Myc',    WDR5(s1=1) % Myc(DVDL=1))
+Observable('WDR5_RbBP5',  WDR5(s1=1) % RbBP5(DVDL=1))
 Observable('WDR5_KANSL2', WDR5(s1=1) % KANSL2(DVDL=1))
-Observable('Myc_free', Myc(DVDL=None))
+
 
     
 

run_WDR5complex.py
@@ -11,57 +11,24 @@ from pysb.bng import run_ssa
 from pysb.integrate import odesolve
 import numpy as np
 import re
-t=linspace(0,1,50)
 
-
-colors = ('r','k','b','g','0.5')
-points = linspace(0.01,1,100) 
+t = linspace(0,5,100)
 ref = np.array([p.value for p in model.parameters])
 
-for i in points:  
-    for j in range(len(model.parameters)):
-        if re.search('_0$', model.parameters[j].name):
-            model.parameters[j].value *= i
-    x=run_ssa(model,t,verbose=False)
-    for j in range(len(model.observables)):
-        obs = model.observables[j]
-        plt.figure(str(obs))
-        plt.legend(loc=0)
-        if i == 0.01:
-            fig = plt.plot(t,x[str(obs)]/np.max(x[str(obs)]),color=colors[j], label=str(obs))
-        else:    
-            fig = plt.plot(t,x[str(obs)]/np.max(x[str(obs)]),color=colors[j])
-    for j in range(len(model.parameters)):
-        model.parameters[j].value = ref[j]
-n_conc = np.zeros((len(model.observables),np.shape(points)[0]))
-counter = 0
-for i in points:
-    for j in range(len(model.parameters)):
-        if re.search('_0$', model.parameters[j].name):
-            model.parameters[j].value *= i
-    x=odesolve(model,t,verbose=False)
-    for j in range(len(model.observables)):
-        obs = model.observables[j]
-        plt.figure(str(obs))
-        plt.legend(loc=0)
-        plt.title(str(model.observables[j].name))
-        n_conc[j,counter] = x[str(obs)][-1]#/np.max(x[str(obs)])
-        if i == 0.01:
-            plt.plot(t,x[str(obs)]/np.max(x[str(obs)]),color=colors[j], label=str(obs))
-            print x[str(obs)]
-        else:    
-            plt.plot(t,x[str(obs)]/np.max(x[str(obs)]),color=colors[j])
-    for j in range(len(model.parameters)):
-        model.parameters[j].value = ref[j]
-    counter +=1
-fig = plt.figure()
-ax1 = fig.add_subplot(5,2,1)
-ax2 = fig.add_subplot(5,2,2)
-ax3 = fig.add_subplot(5,2,3)
+# Run simulations
+x = odesolve(model,t,verbose=True)
+y = run_ssa(model,t,seed=100,verbose=True)
 
-
-for j in range(len(model.observables)):
-    plt.title(str(model.observables[j].name))
-    for i in range(len(points)):
-        plt.plot(points[i], n_conc[j,i], '.',color=colors[j])
-    plt.show()
+# Plot observables (each in a different figure)
+for obs in model.observables:
+    plt.figure(str(obs))
+    plt.plot(t[:len(x[str(obs)])],x[str(obs)],lw=3,label=str(obs)+" (ODE)")
+    plt.plot(t[:len(y[str(obs)])],y[str(obs)],lw=2,label=str(obs)+" (SSA)")
+    plt.legend(loc=0)
+    plt.xlim(xmax=t[-1])
+    plt.xlabel("Time")
+    plt.ylabel("Copy number")
+    plt.ticklabel_format(useOffset=False)
+    plt.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
+        
+plt.show()