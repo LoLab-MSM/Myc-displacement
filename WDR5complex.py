5ebe6e65329b27f839c7d658610bcd674352777d
Leonard A. Harris <lh64@cornell.edu>
2015-02-08 03:30:48 -0600

    Numerous changes to WDR5complex.py and run_WDR5complex.py to make it easier to change the system volume and initial protein levels.

--- WDR5complex.py
+++ WDR5complex.py
@@ -4,40 +4,65 @@ from numpy import linspace
 import matplotlib.pyplot as plt
 from pysb.bng import run_ssa
 from pysb.integrate import odesolve
-import scipy.constants as sc
+from scipy.constants import N_A
+from sympy import sympify
 
 Model() 
+
+# Parameter('test1', 1.)
+# Parameter('test2', 1.)
+# Parameter('test3', 1.)
+# Parameter('test4', 1.)
+# Expression('TEST1', sympify("test1*test2"))
+# Expression('TEST2', sympify("TEST1*test3"))
+# Expression('TEST3', sympify("TEST2*test4"))
+
+# Cell volume (1 picoliter is typical for eukaryotes)
+Parameter('VOL', 1e-12) # L 
+
+# Initial protein copy numbers (estimates from Prof. Tansey)
+Parameter('WDR5_0',   16000) # molec
+Parameter('Myc_0',    5000)  # molec
+Parameter('RbBP5_0',  4500)  # molec
+Parameter('KANSL2_0', 2000)  # molec
+
+def set_volume(vol):
+    WDR5_0.value   *= (vol/VOL.value)
+    Myc_0.value    *= (vol/VOL.value)
+    RbBP5_0.value  *= (vol/VOL.value)
+    KANSL2_0.value *= (vol/VOL.value)
+    VOL.value       = vol
+
+def set_initial_conditions(wdr5=None, myc=None, rbbp5=None, kansl2=None, conc=False):
+    for (X,y) in [(WDR5_0, wdr5), (Myc_0, myc), (RbBP5_0, rbbp5), (KANSL2_0, kansl2)]:
+        if (y != None):
+            if conc: 
+                y *= N_A * VOL.value # M -> molec
+            X.value   = y   # molec
+    
+# Dissocation constants (from Prof. Tansey)
+Parameter('Kd_WDR5_Myc',    2.7*1e-6) # uM -> M
+Parameter('Kd_WDR5_RbBP5',  5.6*1e-6) # uM -> M
+Parameter('Kd_WDR5_KANSL2', 8.6*1e-6) # uM -> M
+
+# Reverse binding rate constants
+Parameter('kr_WDR5_Myc',    100) # /time
+Parameter('kr_WDR5_RbBP5',  100) # /time
+Parameter('kr_WDR5_KANSL2', 100) # /time
+
+# Forward binding rate constants
+Expression('kf_WDR5_Myc',    sympify("kr_WDR5_Myc    / Kd_WDR5_Myc    / %g / VOL" % N_A)) # /M-time -> /time
+Expression('kf_WDR5_RbBP5',  sympify("kr_WDR5_RbBP5  / Kd_WDR5_RbBP5  / %g / VOL" % N_A)) # /M-time -> /time
+Expression('kf_WDR5_KANSL2', sympify("kr_WDR5_KANSL2 / Kd_WDR5_KANSL2 / %g / VOL" % N_A)) # /M-time -> /time
 
 # Binding domain names are taken from the summary model found in the 
 # Dias, Nguyen, Georgiev et al., 2014 article
 
-VOL = 1e-12 # L (1 picoliter is typical volume of eukaryotic cell)
-
+# Monomers
 Monomer('WDR5',   ['s1'])
 Monomer('Myc',    ['DVDL'])
 Monomer('RbBP5',  ['DVDL'])
 Monomer('KANSL2', ['DVDL'])
-
-# Dissocation constants (from experiment)
-Kd_WDR5_Myc    = 2.7*1e-6*sc.N_A*VOL # uM -> molecules
-Kd_WDR5_RbBP5  = 5.6*1e-6*sc.N_A*VOL # uM -> molecules
-Kd_WDR5_KANSL2 = 8.6*1e-6*sc.N_A*VOL # uM -> molecules
-
-# Forward binding rate constants in units of /s
-Parameter('kf_WDR5_Myc',    1e-6)
-Parameter('kf_WDR5_RbBP5',  1e-6)
-Parameter('kf_WDR5_KANSL2', 1e-6)
-
-# Reverse binding rates are in units of /s
-Parameter('kr_WDR5_Myc',    kf_WDR5_Myc.value    * Kd_WDR5_Myc)
-Parameter('kr_WDR5_RbBP5',  kf_WDR5_RbBP5.value  * Kd_WDR5_RbBP5)
-Parameter('kr_WDR5_KANSL2', kf_WDR5_KANSL2.value * Kd_WDR5_KANSL2)
-
-# The initial protein levels (uM -> molecules) are set to the estimates provided by Dr. Tansey. 
-Parameter('WDR5_0',   round(2.657e-02*1e-6*sc.N_A*VOL)) # 16000
-Parameter('Myc_0',    round(8.303e-03*1e-6*sc.N_A*VOL)) # 5000
-Parameter('RbBP5_0',  round(7.473e-03*1e-6*sc.N_A*VOL)) # 4500
-Parameter('KANSL2_0', round(3.321e-03*1e-6*sc.N_A*VOL)) # 2000
 
 # Initial conditions
 Initial(WDR5(s1=None),     WDR5_0)
@@ -51,17 +76,63 @@ Rule('WDR5_binds_RbBP5',  WDR5(s1=None) + RbBP5(DVDL=None)  <> WDR5(s1=1) % RbBP
 Rule('WDR5_binds_KANSL2', WDR5(s1=None) + KANSL2(DVDL=None) <> WDR5(s1=1) % KANSL2(DVDL=1), kf_WDR5_KANSL2, kr_WDR5_KANSL2)
 
 # Observables
+
 # Free species
 Observable('WDR5_free',   WDR5(s1=None))
 Observable('Myc_free',    Myc(DVDL=None))
 Observable('RbBP5_free',  RbBP5(DVDL=None))
 Observable('KANSL2_free', KANSL2(DVDL=None))
+
 # Complexes
 Observable('WDR5_Myc',    WDR5(s1=1) % Myc(DVDL=1))
 Observable('WDR5_RbBP5',  WDR5(s1=1) % RbBP5(DVDL=1))
 Observable('WDR5_KANSL2', WDR5(s1=1) % KANSL2(DVDL=1))
-
-
-    
+   
+######################################################################################################
+# From: "Tansey, William P." <william.p.tansey@Vanderbilt.Edu>
+# To: "Lopez, Carlos F" <c.lopez@Vanderbilt.Edu>
+# Subject: Myc
+# Date: October 22, 2014 at 9:16:35 AM CDT
+#
+# Hi Carlos,
+# 
+# Enjoyed our chat yesterday, as always. I think that we will be able to generate some juicy data 
+# for modeling as we develop better assays for MYC transcription etc. moving forward. In the meantime, 
+# here's a recap of the more immediate challenge we discussed yesterday. As yet, none of this is 
+# published so please keep to yourself/group.
+# 
+# In a nutshell, we've found that MYC needs to interact with WDR5 to bind chromatin and work as a 
+# transcription factor. WDR5 is present in MLL-type histone methyltransferases, where it binds to 
+# many proteins, including RBBP5. WDR5 is also present in the NSL histone acetyltransferase complex, 
+# where it binds many proteins, including KANSL2. The RBBP5-WDR5 and KANSL2-WDR5 interfaces are 
+# virtually identical to each other, and this has been argued as an important mechanism that directs 
+# WDR5 into one complex and not the other.
+# 
+# As mentioned yesterday, MYC binds WDR5 in exactly the same way as does RBBP5 and KANSL2 (picture 
+# below and more detail in attached papers). 
+# 
+# Our model is that MYC displaces RBBP5 and/or KANSL2 from the MLL and/or NSL complexes by direct 
+# competition for binding. What would be fun to determine, is whether this is possible, and what the 
+# relative levels of MYC, WDR5, RBBP5, and KANSL2 would need to be to achieve a certain % of replacement.
+# 
+# The relevant Kds are
+# 
+# MYC:WDR5 2.7 uM
+# RBBP5:WDR5 5.6 uM
+# KANSL2:WDR5 8.6 uM.
+# 
+# Cellular concentrations are harder to pin down. For MYC, we can say that normal levels are 5,000 molecules 
+# per cell. Typical overexpression is 75,000 molecules per cell. But the numbers can go as high as 500,000 
+# molecules per cell, depending on experimental setup. For WDR5, the attached paper gives 16,000 molecules 
+# per U2OS cell (I think that's on the low side), 4,500 molecules per cell for RBBP5, and they don't see KANSL2. 
+# Bottom line is that these numbers probably all need to be measured experimentally in our system to be able to 
+# get something we can believe.
+# 
+# Let me know if you need anything else!
+# 
+# All the best,
+# 
+# Bill
+######################################################################################################
 
 
--- run_WDR5complex.py
+++ run_WDR5complex.py
@@ -5,6 +5,8 @@ Created on Wed Nov 19 13:11:44 2014
 @author: marion
 """
 from WDR5complex import model
+from WDR5complex import set_volume
+from WDR5complex import set_initial_conditions
 from numpy import linspace
 import matplotlib.pyplot as plt
 from pysb.bng import run_ssa
@@ -12,23 +14,67 @@ from pysb.integrate import odesolve
 import numpy as np
 import re
 
-t = linspace(0,5,100)
-ref = np.array([p.value for p in model.parameters])
+run_ode = True
+
+# set_volume(10.*model.parameters['VOL'].value)
+# set_initial_conditions(myc=100.*model.parameters['Myc_0'].value)
+
+t = linspace(0,5,101)
+# ref = np.array([p.value for p in model.parameters])
 
 # Run simulations
-x = odesolve(model,t,verbose=True)
-y = run_ssa(model,t,seed=100,verbose=True)
+x = run_ssa(model,t,seed=100,verbose=True)
+if run_ode:
+    y = odesolve(model,t,verbose=True)
+
+print
+for ic in model.initial_conditions:
+    print "%s: %g" % (ic[1].name, ic[1].value)
+print
+for p in model.parameters_rules():
+    print "%s: %g" % (p.name, p.value)
+print
+for e in model.expressions:
+    subs = [(p.name, p.value) for p in model.parameters]
+    print "%s: %s " % (e.name, e.expand_expr(model).subs(subs))
 
 # Plot observables (each in a different figure)
+
+#Free species
+fig_free, axs_free = plt.subplots(4, 1, sharex=True)
+fig_free.canvas.set_window_title('Free species')
+n=0
+
+# Complexes
+fig_cpx, axs_cpx = plt.subplots(3, 1, sharex=True)
+fig_cpx.canvas.set_window_title('Complexes')
+m=0
+
+def change_yticks(mult, old_ticks):
+    new_ticks = [old_ticks[0]]
+    while new_ticks[-1] < old_ticks[-1]:
+        new_ticks.append( new_ticks[-1] + mult*(old_ticks[1]-old_ticks[0]) )
+    return new_ticks
+
+def plot_fig(axs):
+    axs.plot(t[:len(x[str(obs)])], x[str(obs)], lw=3, label=str(obs)+" (SSA)", color='blue')
+    if run_ode:
+        axs.plot(t[:len(y[str(obs)])], y[str(obs)], lw=2, label=str(obs)+" (ODE)", color='red')
+    axs.legend(loc=0)
+    axs.set_xlim(xmax=t[-1])
+    axs.set_xlabel("Time")
+    axs.set_ylabel("Copy number")
+    axs.ticklabel_format(useOffset=False)
+    axs.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
+    # Double the distance between the y-ticks
+    axs.yaxis.set_ticks(change_yticks(2., axs.yaxis.get_ticklocs()))
+
 for obs in model.observables:
-    plt.figure(str(obs))
-    plt.plot(t[:len(x[str(obs)])],x[str(obs)],lw=3,label=str(obs)+" (ODE)")
-    plt.plot(t[:len(y[str(obs)])],y[str(obs)],lw=2,label=str(obs)+" (SSA)")
-    plt.legend(loc=0)
-    plt.xlim(xmax=t[-1])
-    plt.xlabel("Time")
-    plt.ylabel("Copy number")
-    plt.ticklabel_format(useOffset=False)
-    plt.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
-        
+    if re.search(r'_free$', str(obs)):
+        plot_fig(axs_free[n])
+        n += 1
+    else:
+        plot_fig(axs_cpx[m])
+        m += 1
+
 plt.show()
